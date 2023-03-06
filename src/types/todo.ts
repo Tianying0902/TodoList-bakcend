d854ad84003dec48ecbb5a93f832428ae090a098
Tianying0902 <Aimee_wang0902@163.com>
2023-03-06 22:12:45 +0800

    [Tianying]feat: declare type of queryapromise

--- src/types/todo.ts
+++ src/types/todo.ts
@@ -1,5 +1,2 @@
-export interface ITodo {
-  id: number;
-  task: string;
-  completed: boolean;
-}
+export type SqlConnection = () => Object;
+export type QueryPromise = (query: string, fn: Function) => Promise<void>;
